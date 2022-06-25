7ae0813b5fd64789037e5acfc8342a8b85e703fa
omerfurkank <omerfurkankilic@yandex.com>
2022-06-25 13:25:44 +0300

    generic repo created

---
+++ Business/Concretes/CarManager.cs
@@ -0,0 +1,28 @@
+﻿using Business.Abstracts;
+using Entities.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concretes
+{
+    public class CarManager : ICarService
+    {
+        public void Add(Car car)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Car car)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Car car)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
