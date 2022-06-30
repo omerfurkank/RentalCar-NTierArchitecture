9f0b7d59bfb43fbd5b65c8c33991487a2a636377
omerfurkank <omerfurkankilic@yandex.com>
2022-06-30 13:31:51 +0300

    fluentvalidation added

---
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Concretes;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CarValidator: AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(c => c.Description).NotEmpty().MinimumLength(2).MaximumLength(50);
+
+        }
+    }
+}
